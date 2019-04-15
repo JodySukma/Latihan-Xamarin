7433565619587759f96ed1ae06dfb3d241a1b491
lJodyl <zody.kyozue@gmail.com>
2019-04-15 14:44:54 +0700

    first commit

---
+++ XamarinIntro/XamarinIntro/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinIntro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!", "The button labeled " + button.Text + " has be clicked", "OK");
+        }
+    }
+}
